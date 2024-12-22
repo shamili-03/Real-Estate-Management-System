48caa548b4053a92313c360a0c29d804b9341a27
Shamili Palapati <150045360+shamili-03@users.noreply.github.com>
2024-12-22 11:04:56 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$con = mysqli_connect("localhost","root","Shamili@03","realestatephp");
+	if (mysqli_connect_errno())
+	{
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+?>
